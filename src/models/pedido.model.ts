d7577f59cee93b4d6b5b34501b3854f698ef8205
yaki2022 <estudianteean@hotmail.com>
2022-10-16 07:01:12 -0500

    Mi primera confirmacion

---
+++ src/models/pedido.model.ts
@@ -0,0 +1,55 @@
+import {belongsTo, Entity, model, property} from '@loopback/repository';
+import {Persona} from './persona.model';
+
+@model()
+export class Pedido extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  id_producto: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  Cantidad: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  Total: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  Estado: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  id_persona: string;
+
+  @belongsTo(() => Persona)
+  personaId: string;
+
+  constructor(data?: Partial<Pedido>) {
+    super(data);
+  }
+}
+
+export interface PedidoRelations {
+  // describe navigational properties here
+}
+
+export type PedidoWithRelations = Pedido & PedidoRelations;
---
+++ src/models/persona.model.ts
@@ -0,0 +1,55 @@
+import {Entity, model, property, hasMany} from '@loopback/repository';
+import {Pedido} from './pedido.model';
+
+@model()
+export class Persona extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Nombres: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Apellidos: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Correo: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Celular: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Clave: string;
+
+  @hasMany(() => Pedido)
+  pedidos: Pedido[];
+
+  constructor(data?: Partial<Persona>) {
+    super(data);
+  }
+}
+
+export interface PersonaRelations {
+  // describe navigational properties here
+}
+
+export type PersonaWithRelations = Persona & PersonaRelations;
